378259cbc75c18288db57ac20df5a22de9609b17
pipo <pipo@flufi.io>
2022-05-22 18:51:03 -0300

    fixed actions

--- main.tf
+++ main.tf
@@ -45,6 +45,7 @@ module "repository_from_template" {
   name        = each.value.name
   description = each.value.description
   #checkov:skip=CKV_GIT_1: The repository must be public
+  #ts:skip=AC_GITHUB_0002
   visibility            = try(each.value.visibility, local.defaults.visibility)
   template_files        = local.template_files
   template_files_prefix = local.template_files_prefix
